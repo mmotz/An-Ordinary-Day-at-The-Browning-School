8ceb139eef664bf41df465d4d248a815275d5dc8
Max Motz <mmotz@browning.edu>
2017-11-21 05:20:41 +0000

    An-Ordinary-Day-at-The-Browning-School

---
+++ myTextBasedGameDialogue.rb
@@ -0,0 +1,142 @@
+
+#Title
+puts "An Ordinary Day at the Browning School"
+puts "By Maximilian Motz"
+
+#Disclaimer
+puts "DISCLAIMER: Every character in this game is a AU version of people at Browning in order for me to alter their personality and name slightly to avoid conflict."
+puts "If you think you weren't accurately portrayed in this game, your wrong cuz its NOT YOU!"
+puts "BTW the player is YOU, not the fictional version of me or otherwise"
+
+#Start
+puts "You awaken"
+puts "Lucky you, its panther day (this is fiction btw) at Browning and you have no classes!"
+puts "Don't get the wrong idea though, your momma's still gonna make you go to school..."
+
+#Bedroom choice
+puts "Your in your room, what do you do?"
+puts "Get dressed"
+puts "Go back to Sleep"
+puts "Go to the Bathroom"
+puts "Go to the Diningroom"
+puts "Leave the Building"
+
+#Clothing choice
+puts "What do you want to wear?"
+puts "Something Plain"
+puts "Something swanky"
+puts "A sweatshirt"
+
+#Getting dressed
+puts "You choose a white shirt and a red tie."
+puts "You choose a bright purple shirt and a bright green sparkly bow tie."
+puts "You put on a red sweatshirt with the Browning logo on it"
+
+#Go back to sleep
+puts "You decide to go back to bed cuz this day is a waste of your time."
+puts "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+puts "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz.........."
+puts "z?"
+puts "You sleep through the whole day and that was that..."
+puts "YOU WIN!"
+puts "YOU GOT THE: skip school 4 sleep ending"
+
+#Go to the Bathroom
+puts "Would you like to:"
+puts "Brush your teeth"
+puts "Take a Shower"
+puts "Go to the toilet"
+puts "leave"
+
+#Brushing teeth
+puts "clean and white and very nice"
+
+#Take a Shower
+puts "Your squeaky clean now"
+
+#Go to the toilet
+puts "Kek"
+
+#Go to the Dining room
+puts "What would you like to eat"
+puts "Milk and cereal"
+puts "Cereal and milk"
+puts "Waffles"
+puts "a singular whole banana"
+puts "Omlette"
+
+#Every food except banana and waffles
+puts "OMNOMNOM"
+
+#Waffles
+puts "DO YOU LIKE WAFFLES?"
+puts "YEAH WE LIKE WAFFLES!"
+
+#Banana
+puts "You 'accidentally' put the whole banana in your throat and choke and die."
+
+#Leave the Building(without getting dressed)
+puts "You get arrested for public nudity"
+
+#Leave the Building(after getting dressed)
+puts "You leave your house and make your way over to Browning."
+puts "Outside the building you find one of your classmates dawdiling."
+puts "What do you do"
+puts "Go into the building"
+puts "Talk to Ren"
+
+#Talk to Ren
+puts "How do you wanna start"
+puts "Say 'HI'"
+puts "Say 'Wut Up!'"
+puts "Ask him about VGDC"
+puts "Give him a pat on the cheek"
+puts "Ask him for access to his spicy meme chat"
+puts "Go into Browning"
+
+#HI or Wut Up!
+puts "He says 'hey...' back"
+
+#Ask him about VGDC
+puts "He says it was cancelled this week and is occuring next week"
+puts "Response?"
+puts "Inquire that we haven't had a meeting in forever"
+puts "Leave it at that"
+
+#Inquire about VGDC
+puts "He admits thats true... but they had good reason for putting it off for this long."
+puts "Whether it be for holiday or testing"
+puts "Response?"
+puts "Offer to run the club for him"
+puts 'Leave it at that'
+
+#Offer to run the club for him
+puts "He declines since he is the 'one and only leader of the club'"
+puts "However, seeing your optimistic attitude about a club no one takes seriously, he makes you copresident #6"
+
+#Give him a pat on the cheek
+puts "He slaps you so hard back that your head explodes"
+
+#Ask him for spicy meme chat
+puts "He declines"
+
+#Ask him for spicy meme chat (repeated)
+puts "He declines again"
+
+#Spicy meme attempt 3
+puts "He mentions that most the 'memes' are only funny because they're controversial."
+
+#persistence
+puts "He gets tired of you and decides to add you"
+puts "Out of nowhere a bunch of cops come out of some bushes and arrest you both."
+
+#Go into Browning
+puts "You walk through those red doors as Rames, Headmaster of the upper school, grabs your hand into the building"
+
+#SweatShirt gameover
+puts "He notices Your wearing a sweatshirt and instantly gets triggered kicking you out of the building forever."
+
+#Hallway dialogue
+puts "your standing in the hallway, free to explore the building as you please"
+puts "What floor do you go to?"
+puts ""
---
+++ myTextBasedGameMechanics.rb
@@ -0,0 +1,18 @@
+class Events 
+    def gameOver
+        #If the player triggers one of the various death scenes, this plays    
+    end
+    
+    def win
+        #If the player triggers one of the various win scenes, this plays
+    end
+    
+    def play
+        #Initiates the game
+    end
+    
+    def item
+        #Plays when the player collects an item or achieves something that allows for alternative roots later in the story
+    end
+    
+end
